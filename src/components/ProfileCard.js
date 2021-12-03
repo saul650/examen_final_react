35626e821b887243e340aefdb85f9e0ff90a5536
saul650 <saulsoto1900@gmail.com>
2021-12-03 09:23:31 -0400

    Profile realizado

---
+++ src/components/ProfileCard.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./ProfileCard.css";
+import avatar from "../images/image-rita.png";
+
+function ProfileCard(props) {
+	return (
+		<div className="card-container">
+			<header>
+				<img src={avatar} alt={props.name} />
+			</header>
+			<h1 className="bold-text">
+				{props.name} <span className="normal-text">{props.age}</span>
+			</h1>
+			<h2 className="normal-text">{props.city}</h2>
+			<div className="social-container">
+				<div className="followers">
+					<h1 className="bold-text">{props.followers}</h1>
+					<h2 className="smaller-text">Yaretas 24</h2>
+				</div>
+				<div className="likes">
+					<h1 className="bold-text">{props.likes}</h1>
+					<h2 className="smaller-text">taretas 24</h2>
+				</div>
+			
+			</div>
+		</div>
+	);
+}
+
+export default ProfileCard;
